829a1b40f4e87218cb968385489bdb4c682cec1d
desancheztorres <desancheztorres@gmail.com>
2019-11-28 18:22:54 +0000

    Rendering an upload list

--- src/uploader/components/Uploader.vue
+++ src/uploader/components/Uploader.vue
@@ -1,20 +1,39 @@
 <template>
-    <UploaderForm
-        @chosen="handleFilesChosen"
-    />
+   <div>
+       <UploaderForm
+               @chosen="handleFilesChosen"
+       />
+       <UploaderFile
+        v-for="(upload, index) in uploads"
+        :key="index"
+        :upload="upload"
+       />
+   </div>
 </template>
 
 <script>
-    import UploaderForm from '@/uploader/components/UploaderForm';
+    import UploaderForm from '@/uploader/components/UploaderForm'
+    import UploaderFile from '@/uploader/components/UploaderFile'
 
     export default {
         components: {
-            UploaderForm
+            UploaderForm,
+            UploaderFile
+        },
+
+        data () {
+            return {
+                uploads: []
+            }
         },
 
         methods: {
             handleFilesChosen (files) {
-                console.log(files)
+                this.uploads.push(...Array.from(files).map((file) => {
+                    return {
+                        file
+                    }
+                }))
             }
         }
     }
---
+++ src/uploader/components/UploaderFile.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-white rounded-lg p-4 flex items-stretch mb-1">
+        <div>
+        </div>
+        <div class="flex flex-col justify-between">
+            <div class="mb-2">
+                <div class="font-medium mr-3 text-gray-700 leading-tight">
+                    {{ upload.file.name }}
+                </div>
+                <div class="text-gray-600 text-sm leading-tight">
+                    2 MB
+                </div>
+            </div>
+
+            <div class="text-gray-600 text-sm align-baseline">
+                Cancelled
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            upload: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+</script>
